c755c834f6329bca42b57d0b6d832014e65d7e3b
Kaito Pangelinan <darkphoenix9734@gmail.com>
2026-09-25 12:53:25 -0700

    Add 201, server-side ids, DELETE endpoint, and ID column

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -11,17 +11,25 @@ function MyApp() {
     return promise;
   }
 
-  function postUser(person) {
-  const promise = fetch("Http://localhost:8000/users", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(person),
-  });
+  // #4: send a DELETE request to the backend
+  function deleteUser(id) {
+    const promise = fetch(`http://localhost:8000/users/${id}`, {
+      method: "DELETE",
+    });
+    return promise;
+  }
 
-  return promise;
-}
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
 
   useEffect(() => {
     fetchUsers()
@@ -32,20 +40,49 @@ function MyApp() {
       });
   }, []);
 
+  // function removeOneCharacter(index) {
+  //   const updated = characters.filter((character, i) => {
+  //     return i !== index;
+  //   });
+  //   setCharacters(updated);
+  // }
+
+  // #4 - delete on the server first, then update the table
   function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
-    });
-    setCharacters(updated);
+    const person = characters[index];
+    deleteUser(person.id)
+      .then((res) => {
+        if (res.status === 204) {
+          const updated = characters.filter((character, i) => i !== index);
+          setCharacters(updated);
+        } else {
+          throw new Error("Failed to delete user");
+        }
+      })
+      .catch((error) => console.log(error));
   }
 
-function updateList(person) {
-  postUser(person)
-    .then(() => setCharacters([...characters, person]))
-    .catch((error) => {
-      console.log(error);
-    });
-}
+  // function updateList(person) {
+  //   postUser(person)
+  //     .then(() => setCharacters([...characters, person]))
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }
+
+  // #1 - only accept 201, #3 - use the object the server returns
+  function updateList(person) {
+    postUser(person)
+      .then((res) => {
+        if (res.status === 201) {
+          return res.json();
+        } else {
+          throw new Error("Failed to create user");
+        }
+      })
+      .then((newUser) => setCharacters([...characters, newUser]))
+      .catch((error) => console.log(error));
+  }
 
   return (
     <div className="container">
